fa190dac31a7624094a1a533e19658abfb6ebc54
anar <anarb2536@gmail.com>
2025-01-19 14:24:06 +0100

    Add project files.

---
+++ test/test.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int getSecondLargest(vector <int>& arr,int first,int second,int index) {
+	if (index == arr.size() - 1) {
+		if (arr[index] > first) {
+			first = arr[index];
+		}
+		else if (arr[index] < first && arr[index] > second) {
+			second = arr[index];
+		}
+		return second;
+	}
+	if (arr[index] > first) {
+		first = arr[index];
+	}
+	else if (arr[index] < first && arr[index] > second) {
+		second = arr[index];
+	}
+	return getSecondLargest(arr, first, second, index + 1);
+}
+
+int main() {
+	vector<int> arr = { 12, 35, 1, 10, 34, 1 };
+	cout << getSecondLargest(arr,-1,-1,0);
+
+	return 0;
+}
---
+++ test/test1.cpp
@@ -0,0 +1,14 @@
+//#include <Windows.h>
+//#include <iostream>
+//
+//using namespace std;
+//
+//int nScreenHeight = 40;
+//int nScreenWidth = 120;
+//
+//int main() {
+//	wchar_t* screen = new wchar_t[nScreenHeight * nScreenWidth];
+//	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+//	SetConsoleActiveScreenBuffer(hConsole);
+//	DWORD dvByteswritten = 0;
+//}
